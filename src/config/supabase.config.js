5c6be5df8feeed467c80081eaeed8dec1385eb4f
Pluem10 <664259010@webmail.npru.ac.th>
2026-02-01 21:01:59 +0700

    NewENV

--- src/config/supabase.config.js
+++ src/config/supabase.config.js
@@ -1,6 +1,12 @@
  const {createClient} = require('@supabase/supabase-js');
 
 
- const supabase = createClient(  process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+ const supabase = createClient(  
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+    , process.env.SUPABASE_SERVICE_ROLE_KEY,
+    process.env.BASE_URL,
+    process.env.DB_URL,
+);
 
     module.exports = { supabase };
